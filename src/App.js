6fa4fd073999aff0f90309dfd6b07f26db0de201
Floyd Noel <floyd.noel@gmail.com>
2020-07-31 14:37:54 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <p>React-Kit Documentation</p>
       </header>
+      <p>my favorite packages and components together, forever.</p>
     </div>
   );
 }
