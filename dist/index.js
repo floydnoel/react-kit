b22067fbf61885d8d390a9a288a6a8c6495c0195
Floyd Noel <floyd.noel@gmail.com>
2020-08-17 08:30:13 -0400

    new dist build

--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,4 @@ import Markdown from 'components/markdown/markdown';
 import TextField from 'components/text-field/text-field';
 import { Router, Switch, Route, Link } from 'components/router/router';
 export { Markdown, TextField, Router, Switch, Route, Link };
+//# sourceMappingURL=index.js.map
